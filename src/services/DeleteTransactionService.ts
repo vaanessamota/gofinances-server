8b723d983fd800a2e1aea00df23434aa45649ea0
Vanessa <vanessaoliveira2706@gmail.com>
2020-07-12 21:49:41 -0300

    delete tx service

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    const txExists = transactionRepository.find({ where: { id } });
+
+    if (!txExists) {
+      throw new AppError('This transaction does not exist', 400);
+    }
+
+    await transactionRepository.delete({ id });
   }
 }
 
