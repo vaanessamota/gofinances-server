60b2686ecc0a7958545cd7ee7bb1f24ec61180c8
Vanessa <vanessaoliveira2706@gmail.com>
2020-07-12 21:49:20 -0300

    transaction repository and create tx service

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +11,32 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    const transactionsRepository = getRepository(Transaction);
+
+    const incomeTransactions = await transactionsRepository.find({
+      where: { type: 'income' },
+    });
+
+    incomeTransactions.forEach(transaction => {
+      income += transaction.value;
+    });
+
+    const outcomeTransactions = await transactionsRepository.find({
+      where: { type: 'outcome' },
+    });
+
+    outcomeTransactions.forEach(transaction => {
+      outcome += transaction.value;
+    });
+
+    const total = income - outcome;
+
+    const balance = { income, outcome, total };
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
-// import AppError from '../errors/AppError';
-
+import { getRepository, getCustomRepository } from 'typeorm';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const customTxRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const categoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    let category_id;
+
+    if (categoryExists) {
+      category_id = categoryExists.id;
+    } else {
+      const newCategory = categoryRepository.create({ title: category });
+
+      await categoryRepository.save(newCategory);
+
+      category_id = newCategory.id;
+    }
+
+    const balance = await customTxRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new AppError(
+        'Cant create transaction with an invalid outcome.',
+        400,
+      );
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
